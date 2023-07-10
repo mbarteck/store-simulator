5888c4dfeb3b872e556b891feaefd9a0857102bc
Mateusz Bartecki <mbartecki@silenteight.com>
2023-07-10 13:57:25 +0200

    Add validation of list of items

--- src/main/java/com/mbartecki/storesimulator/dto/OrderRequest.java
+++ src/main/java/com/mbartecki/storesimulator/dto/OrderRequest.java
@@ -2,9 +2,10 @@ package com.mbartecki.storesimulator.dto;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.util.List;
 
 public record OrderRequest(@Email(message = "Invalid email address") String userEmail,
-                           @Valid List<CheckoutItem> items) {
+                           @NotEmpty(message = "List of items cannot be empty") @Valid List<CheckoutItem> items) {
 }
