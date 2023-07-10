e65165e0c740bd9295fd3f6e080ddc557df78db8
Mateusz Bartecki <mbartecki@silenteight.com>
2023-07-10 06:57:54 +0200

    Fix e-mail re-try

--- src/main/java/com/mbartecki/storesimulator/consumers/SendEmailConsumer.java
+++ src/main/java/com/mbartecki/storesimulator/consumers/SendEmailConsumer.java
@@ -1,8 +1,16 @@
 package com.mbartecki.storesimulator.consumers;
 
+import lombok.extern.slf4j.Slf4j;
+
 import com.mbartecki.storesimulator.dto.EmailDto;
 import com.mbartecki.storesimulator.port.EmailSenderPort;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.retrytopic.TopicSuffixingStrategy;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.serializer.DeserializationException;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
@@ -10,6 +18,7 @@ import org.springframework.stereotype.Component;
 import static com.mbartecki.storesimulator.utils.JsonUtils.deserialize;
 
 @Component
+@Slf4j
 public class SendEmailConsumer {
 
   private final EmailSenderPort emailSenderPort;
@@ -18,10 +27,19 @@ public class SendEmailConsumer {
     this.emailSenderPort = emailSenderPort;
   }
 
+  @RetryableTopic(
+      attempts = "5",
+      topicSuffixingStrategy = TopicSuffixingStrategy.SUFFIX_WITH_INDEX_VALUE,
+      backoff = @Backoff(delay = 1000, multiplier = 2.0),
+      exclude = { DeserializationException.class}
+  )
   @KafkaListener(topics = "send-email-topic")
-  @Retryable(maxAttempts = 3, backoff = @Backoff(delay = 1000, multiplier = 2.0))
   public void receiveEmailDto(String jsonString) {
     EmailDto emailDto = deserialize(jsonString, EmailDto.class);
     emailSenderPort.sendEmail(emailDto);
+  }
+  @DltHandler
+  public void handleDlt(String message, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+    log.info("Message: {} handled by dlq topic: {}", message, topic);
   }
 }
